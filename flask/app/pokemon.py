48f514a64a844f5842a1b56abe0dae4fae721c5c
GeorgyMironov2001 <mironov.ga@phystech.edu>
2023-06-07 02:07:25 +0300

    abilities

--- flask/app/pokemon.py
+++ flask/app/pokemon.py
@@ -4,17 +4,6 @@ import requests
 
 app = Flask(__name__)
 
-
-@app.route('/numberinfo/<number>', methods=['GET'])
-def get_info_about_number(number):
-    import logging
-    logging.basicConfig(filename='error.log', level=logging.DEBUG)
-    N = str(int(number) + 1)
-    if not str(number).isdecimal():
-        return f'<p1>{number} is not a decimal number</p1>'
-    return requests.get(f"http://numbersapi.com/{N}").content
-
-
 def collect_together(feature_list):
     res = ""
     for s in feature_list:
@@ -22,7 +11,7 @@ def collect_together(feature_list):
     return res
 
 
-def get_features(data):
+def get_pokemon_features(data):
     height = "height: "
     weight = "weight: "
     abilities = "abilities: "
@@ -44,8 +33,20 @@ def get_info_about_pokemon(name):
     if response.status_code != 200:
         return "Wrong pokemon`s name"
     data = response.json()
-    return get_features(data)
+    return get_pokemon_features(data)
 
+@app.route('/WhoHasAbility/<name>', methods=['GET'])
+def get_info_about_ability(name):
+    import logging
+    logging.basicConfig(filename='error.log', level=logging.DEBUG)
+    response = requests.get(f"https://pokeapi.co/api/v2/ability/{name}")
+    if response.status_code != 200:
+        return "Wrong ability name"
+    data = response.json()
+    pokemons = "pokemons: "
+    for pokemon in data['pokemon']:
+        pokemons += pokemon['pokemon']['name'] + " "
+    return pokemons
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5001))
